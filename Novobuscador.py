9b91332908b94e69d5a46a6d84514b32e34cf2b0
denidantas <denidantas1975@outlook.com>
2023-03-21 12:47:27 -0300

    localizção da coluna e altualização da cotação

--- Novobuscador.py
+++ Novobuscador.py
@@ -1,10 +1,7 @@
-import requests
+#import requests
+import PySimpleGUI as sg
 
-cot = dolar = euro = btc = 0
-
-cot = int(input("Informe: 1 - Cotação Dólar   2 - Cotação Euro   3 - cotação BitC: "))
-
-lass TelaPython:
+class TelaPython:
     def __init__(self):
         layout = [
             [sg.Checkbox('Dolar', key='Dolar'), sg.Checkbox('Euro', key='Euro'), sg.Checkbox('BitCoins', key='BTC')],
@@ -37,39 +34,3 @@ lass TelaPython:
             print(f'Informe a coluna do valor de compra original: {loc_val_original}')
 tela = TelaPython()
 tela.Iniciar()
-def pegar_cotacoes(cot):
-    requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
-    requisicao_dic = requisicao.json()
-
-    if cot == 1:
-        mo_e_da = "Dólar"
-        return requisicao_dic["USDBRL"]["bid"]
-
-    if cot == 2:
-        mo_e_da = "Euro"
-        return requisicao_dic["EURBRL"]["bid"]
-
-    if cot == 3:
-        mo_e_da = "BitC"
-        return requisicao_dic["BTCBRL"]["bid"]
-
-
-cotacao = pegar_cotacoes(cot)
-
-import pandas as pd
-
-dados = input("Informe o endereço da tabela Excel que deseja atualizar valores: ")
-tabela = pd.read_excel(dados)
-pd.set_option('Display.max_columns', None)
-print(tabela)
-coluna = input("Informe nome da coluna dos valores da moeda a ser alterada: ")
-col2 = input("Informe o nome dacoluna que contém os valores da moeda desejada:")
-col3 = input("Informe a coluna do valor de compra em Reais: ")
-col4 = input("informa a coluna que contém o valor na moeda original: ")
-# tabela.loc[tabela[(coluna)] == "Dólar","Cotação"] = float(cotacao)
-# tabela["Preço Base Reais"] = tabela["Cotação"] * tabela["Preço Base Original"]
-# tabela["Preço Final"] = tabela["Preço Base Reais"] * tabela["Ajuste"]
-
-tabela.loc[tabela[coluna] == mo_e_da, col2] = float(cotacao)
-tabela[col3] = tabela[col2] * tabela[col4]
-print(tabela)
